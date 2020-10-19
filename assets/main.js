02295060907082056bdb7766c7edcdc2fb428d2c
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-10-19 16:07:22 +0200

    used indexof

--- assets/main.js
+++ assets/main.js
@@ -5,13 +5,20 @@ var cognomeUtente = prompt("Inserisci il tuo Cognome");
 listaCognomi.push(cognomeUtente);
 //ordine alfabetico
 listaCognomi.sort();
+// document.getElementById('unordered').innerHTML = listaCognomi;
 
 // Stampare la lista dei cognomi in ordine alfabetico e comunicare all'utente la posizione in cui si trova il suo cognome (dopo averlo riordinato).
 //stampa lista in ordine alfabetico
 console.log(listaCognomi);
-for (var i = 0; i < listaCognomi.length; i++) {
-    var currentPosition = listaCognomi[i];
-    if (currentPosition == cognomeUtente) {
-        console.log(cognomeUtente + " la tua posizione è " + (i + 1));
-    }
-}
+
+//first method to find the position in array
+// for (var i = 0; i < listaCognomi.length; i++) {
+//     var currentPosition = listaCognomi[i];
+//     if (currentPosition == cognomeUtente) {
+//         console.log(cognomeUtente + " la tua posizione è " + (i + 1));
+//     }
+// }
+
+//second method
+var position = listaCognomi.indexOf(cognomeUtente);
+console.log(cognomeUtente + " You're " + (position + 1)  + "th in the list");
